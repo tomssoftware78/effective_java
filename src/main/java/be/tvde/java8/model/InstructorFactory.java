35ea7daedf34ea1d514863d4c0cb08d9f8fe4fd5
Tom Van den Eynde <tom.van.den.eynde@ubench.com>
2020-12-25 11:02:12 +0100

    instructor factory

--- src/main/java/be/tvde/java8/model/InstructorFactory.java
+++ src/main/java/be/tvde/java8/model/InstructorFactory.java
@@ -3,6 +3,10 @@ package be.tvde.java8.model;
 import java.util.List;
 
 public interface InstructorFactory {
-    Instructor getInstructor(String name, int yearsOfExperience, String title,
-                                             String gender, boolean onlineCourse, List<String> courses);
+    Instructor getInstructor(String name,
+                             int yearsOfExperience,
+                             String title,
+                             String gender,
+                             boolean onlineCourse,
+                             List<String> courses);
 }
