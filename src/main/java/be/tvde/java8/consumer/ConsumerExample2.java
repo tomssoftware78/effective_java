8941fc57d9a1ba5014fc63410c248ba0be813a2b
Tom Van den Eynde <tom.van.den.eynde@ubench.com>
2020-12-25 11:01:47 +0100

    Consumer example

---
+++ src/main/java/be/tvde/java8/consumer/ConsumerExample2.java
@@ -0,0 +1,30 @@
+package be.tvde.java8.consumer;
+
+import java.util.function.Consumer;
+import be.tvde.java8.model.Instructor;
+import be.tvde.java8.model.Instructors;
+
+public class ConsumerExample2 {
+
+   public static void main(String[] args) {
+      Consumer<Instructor> c = (instructor) -> {
+         System.out.println(instructor);
+      };
+      Instructors.getAll().forEach(c);
+      System.out.println("------------------");
+      Consumer<Instructor> c1 = (instructor) -> {
+         System.out.println(instructor.getName());
+      };
+      Instructors.getAll().forEach(c1);
+
+      System.out.println("------------------");
+      Instructors.getAll().forEach(c.andThen(c1));
+      System.out.println("------------------");
+      Instructors.getAll().forEach((instructor) -> {
+            if(instructor.getYearsOfExperience() > 10) {
+               c1.accept(instructor);
+            }
+      });
+
+   }
+}
