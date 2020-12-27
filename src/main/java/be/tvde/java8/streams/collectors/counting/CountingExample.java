ab95199828b7c8433ad6d21e4fa552d19fcbb014
Tom Van den Eynde <tom.van.den.eynde@ubench.com>
2020-12-27 09:57:54 +0100

    counting on a stream

---
+++ src/main/java/be/tvde/java8/streams/collectors/counting/CountingExample.java
@@ -0,0 +1,23 @@
+package be.tvde.java8.streams.collectors.counting;
+
+import java.util.stream.Collectors;
+import be.tvde.java8.model.Instructors;
+
+public class CountingExample {
+
+   public static void main(String[] args) {
+      //counting numbers of instructors who teach online courses
+      final Long nrTeachingOnline = Instructors.getAll()
+                                      .stream()
+                                      .filter(instructor->instructor.isOnlineCourses())
+                                      .collect(Collectors.counting());
+      System.out.println(nrTeachingOnline);
+
+      final long nrTeachingOnline1 = Instructors.getAll()
+                                    .stream()
+                                    .filter(instructor->instructor.isOnlineCourses())
+                                    .count();
+      System.out.println(nrTeachingOnline1);
+
+   }
+}
---
+++ src/main/java/be/tvde/java8/streams/collectors/counting/readme.txt
@@ -0,0 +1,8 @@
+counting() method in the Collectors class counts the number of elements in a Stream
+
+   Stream.of(1, 2, 3, 4, 5).collect(Collectors.counting());
+
+   Stream.of(1, 2, 3, 4, 5).count();
+
+Helpful in downstream operations
+   groupingBy(String::length, counting());
